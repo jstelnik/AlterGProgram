fbf2fb51afa1554679fcd3100f26a3cc38c5d3d1
jstelnik <jstelni1@jh.edu>
2025-02-26 23:23:54 -0500

    starting src over

--- server/src/models/workoutModel.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, serial, text, integer } from "drizzle-orm/pg-core";
-
-export const workouts = pgTable("workouts", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  duration: integer("duration").notNull(),
-});
